5e3307e22517f71e7aab7ced58abe904b3beda8b
Bakhodir <250350@newuu.uz>
2025-09-25 10:12:05 +0500

    New two problems for lab4

---
+++ p1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x;
+    cout << "Please enter a number: ";
+    cin >> x;
+    if (x>0)
+        cout << "Positive";
+    else {
+        cout << x << " is a negative number" << endl;
+    }
+}
+
+
+
+
--- p2.cpp
+++ p2.cpp
@@ -1,3 +1,20 @@
-//
-// Created by Baxodirjon Murodillayev on 25/09/25.
-//
+#include <iostream>
+using namespace std;
+int main() {
+    int x;
+    cout << "Please enter a number: ";
+    cin >> x;
+    if (x>0 && x%2==0){
+        cout << "Positive and even";
+    }else if (x<0 && x%2!=0) {
+        cout << x << " Negative and odd" << endl;
+    }else if (x>0 && x%2!=0) {
+        cout << x << " Positive and odd" << endl;
+    }else {
+        cout << x << " Negative and even" << endl;
+    }
+}
+
+
+
+
